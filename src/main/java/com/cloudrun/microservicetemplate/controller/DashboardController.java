babb655b9b3c53a93f8b08994eeb8a38765fdd2c
cx.li <chunxialims@163.com>
2024-12-27 12:28:38 -0800

    dashboard

---
+++ src/main/java/com/cloudrun/microservicetemplate/controller/DashboardController.java
@@ -0,0 +1,89 @@
+package com.cloudrun.microservicetemplate.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/dashboard")
+public class DashboardController {
+
+    // @Autowired
+    // private StatusIndicatorService statusIndicatorService;
+    //
+    // @Autowired
+    // private MotorService motorService;
+    //
+    // @Autowired
+    // private BatteryService batteryService;
+
+    @GetMapping("/test")
+    public ResponseEntity<Map<String, String>> test() {
+        Map<String, String> response = new HashMap<>();
+        response.put("data", "hello world");
+        return ResponseEntity.ok(response);
+    }
+
+    // // ======================
+    // // Status Indicators
+    // // ======================
+    // @GetMapping("/icons")
+    // public ResponseEntity<List<Icon>> getIcons() {
+    //     List<Icon> icons = statusIndicatorService.getStatusIndicators();
+    //     return ResponseEntity.ok(icons);
+    // }
+    //
+    // // ======================
+    // // Motor
+    // // ======================
+    // @GetMapping("/motor")
+    // public ResponseEntity<Motor> getMotorDetails() {
+    //     Motor motor = motorService.getMotorDetails();
+    //     return ResponseEntity.ok(motor);
+    // }
+    //
+    // @GetMapping("/motor/gear-ratio")
+    // public ResponseEntity<String> getGearRatio() {
+    //     String gearRatio = motorService.getGearRatio();
+    //     return ResponseEntity.ok(gearRatio);
+    // }
+    //
+    // @PostMapping("/motor/slider")
+    // public ResponseEntity<String> updateMotorSpeed(@RequestParam int speedSetting) {
+    //     motorService.updateMotorSpeed(speedSetting);
+    //     return ResponseEntity.ok("Motor speed updated");
+    // }
+    //
+    // // ======================
+    // // Gauges (Power & RPM)
+    // // ======================
+    // @GetMapping("/gauges/power")
+    // public ResponseEntity<Double> getPowerConsumption() {
+    //     Double power = motorService.calculatePowerConsumption();
+    //     return ResponseEntity.ok(power);
+    // }
+    //
+    // @GetMapping("/gauges/rpm")
+    // public ResponseEntity<Integer> getMotorRPM() {
+    //     Integer rpm = motorService.getCurrentRPM();
+    //     return ResponseEntity.ok(rpm);
+    // }
+    //
+    // // ======================
+    // // Battery
+    // // ======================
+    // @GetMapping("/battery")
+    // public ResponseEntity<Battery> getBatteryDetails() {
+    //     Battery battery = batteryService.getBatteryDetails();
+    //     return ResponseEntity.ok(battery);
+    // }
+    //
+    // @PostMapping("/battery/charging")
+    // public ResponseEntity<String> updateChargingState(@RequestParam boolean charging) {
+    //     batteryService.updateChargingState(charging);
+    //     return ResponseEntity.ok("Charging state updated");
+    // }
+}
